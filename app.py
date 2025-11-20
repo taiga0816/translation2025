5663f44fb94f741a73b6db9fc33df80912917296
taiga0816 <141841314+taiga0816@users.noreply.github.com>
2025-11-20 05:55:49 +0000

    完成

--- app.py
+++ app.py
@@ -11,14 +11,15 @@ model = genai.GenerativeModel("gemini-flash-latest")
 st.title("みんなでつくるAI翻訳アプリ") # タイトル
 
 source_text = st.text_area("翻訳したいテキストを入力してください") # テキスト入力欄
-target_lang = st.selectbox("翻訳先の言語", []) # セレクトボックス
-submit_button = st.button("〇〇〇") # ボタン
+target_lang = st.selectbox("翻訳先の言語", ["英語","モンゴル語","ネパール語"]) # セレクトボックス
+submit_button = st.button("翻訳") # ボタン
 
 # --- ボタンが押された後の処理 ---
 if submit_button and source_text:
     # Geminiへの命令文を作成
-    
+    prompt = f"{target_lang}に翻訳してください:\n{source_text}"
     # APIを呼び出し
-    
+    responce = model.generate_content(prompt)
     # 結果を表示
     st.subheader("翻訳結果")
+    st.write(responce.text)
